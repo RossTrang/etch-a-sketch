7af3252d94a191e9d0ac32dec54ac90af8a8c41a
Ross Parkhill <rossrtw2010@gmail.com>
2018-08-24 14:48:08 +0700

    Basic framework in place

---
+++ script.js
@@ -0,0 +1,11 @@
+/* This is the code to create the drawing grid.
+Initial values will be set to 16x16.
+The 'Clear' button will also prompt the user to enter dimensions for a new grid. */
+
+$(document).ready(function) {
+
+  // Populate the sketchpad
+
+  const sketchpad = document.getElementById('sketchPad')
+
+}
