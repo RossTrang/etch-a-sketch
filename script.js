c05fd75f831897ec4546da84de7005c69e52c2b9
Ross Parkhill <rossrtw2010@gmail.com>
2018-08-25 10:23:14 +0700

    Dynamic grid and CSS  rule creation

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 Initial values will be set to 16x16.
 The 'Clear' button will also prompt the user to enter dimensions for a new grid. */
 
-$(document).ready(function) {
+let dimensions = 16;
 
-  // Populate the sketchpad
+document.body.onload = createGrid(dimensions);
 
-  const sketchpad = document.getElementById('sketchPad')
+function createGrid () {
 
+  const sketchPad = document.createElement('div')
+  sketchPad.className = 'sketchPad';
+
+  for (let index = 0; index < dimensions * dimensions; index++) {
+    var gridCell = document.createElement('div')
+    gridCell.className = 'gridCell';
+    sketchPad.appendChild(gridCell);
+  }
+
+  const sheet = document.createElement('style')
+  sheet.innerHTML = ".sketchPad {display: grid; grid-template-rows: repeat("+dimensions+", 1fr); grid-template-columns: repeat("+dimensions+", 1fr); height: 70vh; width: 70vw; margin: auto; border: 1px black solid}";
+  document.body.appendChild(sheet);
+
+  var currentDiv = document.getElementById('resetBtn'); 
+  document.body.insertBefore(sketchPad, currentDiv); 
 }
